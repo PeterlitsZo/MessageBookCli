ba2e0a9fd8c9a41aeebc147a1ba021d6cde9fbed
Peterlits Zo <peterlitszo@outlook.com>
2020-04-26 21:14:57 +0800

    feat: use units::repr to get pretty output

--- src/units.cpp
+++ src/units.cpp
@@ -4,10 +4,19 @@
 #include <string>
 #include <regex>
 #include <iostream>
+#include <strstream>
+#include <vector>
 
 using std::string;
+using std::strstream;
+using std::vector;
+using std::regex;
 
 namespace units {
+
+// ----------------------------------------------------------------------------
+// ---[ function for check ]---------------------------------------------------
+// ----------------------------------------------------------------------------
 
 bool is_digit(char ch) {
     if(ch >= '0' && ch <= '9') {
@@ -30,7 +39,7 @@ bool is_digit(string str) {
 
 bool is_email(string str) {
     // if the format is like "abc@abc.abc"
-    std::regex EmailPattern(R"(^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$)");
+    regex EmailPattern(R"(^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$)");
     std::match_results<string::const_iterator> Result;
     if(std::regex_match(str, Result, EmailPattern)) {
         return true;
@@ -43,6 +52,56 @@ bool is_not_empty(string str) {
     return str.size() != 0;
 }
 
+// ----------------------------------------------------------------------------
+// ---[ function for repr-output ]---------------------------------------------
+// ----------------------------------------------------------------------------
+
+string repr(string str) {
+    string result;
+    result += "\'";
+
+    for (auto it = str.begin(); it != str.end(); ++it) {
+        int temp_int;
+        switch(*it) {
+        case '\\':
+            result += "\\\\";
+            break;
+        case '\'':
+            result += "\\\'";
+            break;
+        case '\"':
+            result += "\\\"";
+            break;
+        case '\n':
+            result += "\\n";
+            break;
+        case '\r':
+            result += "\\r";
+        case '\t':
+            temp_int = (4 - result.size())?(4 - result.size()):4;
+            result += string(temp_int, ' ');
+            break;
+        }
+    }
+
+    result += "\'";
+    return result;
+}
+
+// ----------------------------------------------------------------------------
+// ---[ function for input ]---------------------------------------------------
+// ----------------------------------------------------------------------------
+vector<string> split(string str) {
+    regex space_re(R"(\s+)");
+    return vector<string>(
+        std::sregex_token_iterator(str.begin(), str.end(), space_re, -1),
+        std::sregex_token_iterator()
+    );
+}
+
+// ----------------------------------------------------------------------------
+// ---[ function for input ]---------------------------------------------------
+// ----------------------------------------------------------------------------
 std::istream& input(string info) {
     std::cout << ' ' << info << " > ";
     return std::cin;
--- src/units.h
+++ src/units.h
@@ -4,17 +4,56 @@
 #include <istream>
 #include <string>
 #include <iostream>
+#include <vector>
+#include <strstream>
 
 namespace units {
 
+// function for check
 bool is_digit(char ch);
 bool is_digit(std::string str);
 bool is_email(std::string str);
 bool is_not_empty(std::string str);
 
+// function for repr-output
+std::string repr(std::string str);
+
+template <typename T>
+std::string repr(T value) {
+    std::strstream ss;
+    ss << value;
+    std::string result;
+    ss >> result;
+
+    return result;
+}
+
+template <typename T>
+std::string repr(T begin, T end) {
+    std::string result;
+    result += "[";
+
+    // loop to repr the content
+    T it;
+    for(it = begin; it != end - 1; ++it) {
+        result += repr(*it);
+        result += " ,";
+    }
+    result += repr(*it);
+
+    result += "]";
+    return result;
+}
+
+// split string
+std::vector<std::string> split(std::string str);
+
+// function for input
 std::istream& input(std::string info);
 
 }
+
+// ----------------------------------------------------------------------------
 
 #endif // for PETERLITS_UNITS_H__
 
