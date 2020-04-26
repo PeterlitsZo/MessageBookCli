38f22c1c7bae2fef78a20f82e46d6f90e553f86f
Peterlits Zo <peterlitszo@outlook.com>
2020-04-26 14:29:19 +0800

    function get_rapidjson_object for value class

--- src/value.h
+++ src/value.h
@@ -2,6 +2,12 @@
 #define PETERLITS_VALUE_H__
 
 #include <iostream>
+#include <string>
+#include <type_traits>
+
+#include "../include/rapidjson/document.h"
+
+using std::string;
 
 template<typename T>
 class Value {
@@ -9,6 +15,8 @@ public:
     Value(T value): is_valid_(true), value_(value){};
     Value(): is_valid_(false), value_(T()){};
     void operator=(T value) {value_ = value; is_valid_ = true;}
+
+    rapidjson::Value* get_rapidjson_value(rapidjson::Document::AllocatorType& allo_ptr);
 
     template<typename S>
     friend std::ostream& operator<<(std::ostream& out, const Value<S>& it);
@@ -18,7 +26,37 @@ private:
     T    value_;
 };
 
-template<typename S>
+template<>
+class Value<string> {
+public:
+    Value(string value): is_valid_(true), value_(value){};
+    Value(): is_valid_(false), value_(""){};
+    void operator=(string value) {value_ = value; is_valid_ = true;}
+
+    rapidjson::Value* get_rapidjson_value(rapidjson::Document::AllocatorType& allo) {
+        rapidjson::Value* v = new rapidjson::Value();
+        v->SetObject();
+        
+        v->AddMember("valid", is_valid_, allo);
+        v->AddMember("value",
+                     rapidjson::Value(value_.c_str(), allo).Move(),
+                     allo);
+        return v;
+    }
+
+    template<typename S>
+    friend std::ostream& operator<<(std::ostream& out, const Value<S>& it);
+
+private:
+    bool      is_valid_;
+    string    value_;
+};
+
+// ----------------------------------------------------------------------------
+// ---[ units part ]-----------------------------------------------------------
+// ----------------------------------------------------------------------------
+
+template <typename S>
 std::ostream& operator<<(std::ostream& out, const Value<S>& it) {
     if (it.is_valid_) {
         out << it.value_;
@@ -28,5 +66,17 @@ std::ostream& operator<<(std::ostream& out, const Value<S>& it) {
     return out;
 }
 
+// [WARNING]: remember to delete it! (should I?)
+template <typename T>
+rapidjson::Value* Value<T>::get_rapidjson_value(
+        rapidjson::Document::AllocatorType& allo) {
+    rapidjson::Value* v = new rapidjson::Value();
+    v->SetObject();
+    
+    v->AddMember("valid", is_valid_, allo);
+    v->AddMember("value", value_, allo);
+    return v;
+}
 
 #endif // for PETERLITS_VALUE_H__
+
