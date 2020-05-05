c47a3606ab380e22d8d1f55e22fdc7ea1608e243
Peterlits Zo <peterlitszo@outlook.com>
2020-05-05 19:29:00 +0800

    stl : (*) delete something that it is useless

--- src/parser/interface.h
+++ src/parser/interface.h
@@ -4,16 +4,9 @@
 #include <string>
 
 #include "lineno.h"
-// #include "../messagebook.h"
 #include "../info.h"
 
 extern Lineno ln;
-
-// std::string help(MessageBook& mb);
-// std::string add (MessageBook& mb);
-// std::string list(MessageBook& mb);
-// 
-// void save(MessageBook& mb);
 
 void print_command(std::string str);
 void print_next_arraw(void);
